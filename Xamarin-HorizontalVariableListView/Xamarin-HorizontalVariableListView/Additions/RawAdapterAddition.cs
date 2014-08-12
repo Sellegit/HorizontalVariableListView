1baecd9e322103236076c3c8cedf2a659f0c5542
Rocky Duan <dementrock@gmail.com>
2014-08-12 15:48:35 -0700

    some random binding stuff

---
+++ Xamarin-HorizontalVariableListView/Xamarin-HorizontalVariableListView/Additions/RawAdapterAddition.cs
@@ -0,0 +1,13 @@
+﻿using Android.Runtime;
+using Android.Widget;
+
+namespace Com.Devsmart.Android.UI {
+
+  public partial class HorizontalListView : AdapterView {
+
+    protected override Java.Lang.Object RawAdapter {
+      get {return Adapter.JavaCast<Java.Lang.Object>();}
+      set {Adapter = value.JavaCast<global::Android.Widget.IListAdapter>();}
+    }
+  }
+}
